181eb7ed188a90a0c93b6c9a4a7b7728bc075078
Kunal Chitalia <kchitalia@internal.salesforce.com>
2015-12-03 12:34:46 -0800

    Add Cordova sample app created via CLI

---
+++ Samples/UserList/platforms/windows/www/cordova_plugins.js
@@ -0,0 +1,109 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/com.salesforce/www/com.salesforce.plugin.oauth.js",
+        "id": "com.salesforce.plugin.oauth"
+    },
+    {
+        "file": "plugins/com.salesforce/www/com.salesforce.plugin.sdkinfo.js",
+        "id": "com.salesforce.plugin.sdkinfo"
+    },
+    {
+        "file": "plugins/com.salesforce/www/com.salesforce.plugin.smartstore.js",
+        "id": "com.salesforce.plugin.smartstore",
+        "clobbers": [
+            "navigator.smartstore"
+        ]
+    },
+    {
+        "file": "plugins/com.salesforce/www/com.salesforce.plugin.sfaccountmanager.js",
+        "id": "com.salesforce.plugin.sfaccountmanager"
+    },
+    {
+        "file": "plugins/com.salesforce/www/com.salesforce.plugin.smartsync.js",
+        "id": "com.salesforce.plugin.smartsync"
+    },
+    {
+        "file": "plugins/com.salesforce/www/com.salesforce.util.bootstrap.js",
+        "id": "com.salesforce.util.bootstrap"
+    },
+    {
+        "file": "plugins/com.salesforce/www/com.salesforce.util.event.js",
+        "id": "com.salesforce.util.event"
+    },
+    {
+        "file": "plugins/com.salesforce/www/com.salesforce.util.exec.js",
+        "id": "com.salesforce.util.exec"
+    },
+    {
+        "file": "plugins/com.salesforce/www/com.salesforce.util.logger.js",
+        "id": "com.salesforce.util.logger"
+    },
+    {
+        "file": "plugins/com.salesforce/www/com.salesforce.util.push.js",
+        "id": "com.salesforce.util.push"
+    },
+    {
+        "file": "plugins/com.salesforce/src/windows/com.salesforce.plugin.oauthProxy.js",
+        "id": "com.salesforce.SalesforceOAuthProxy",
+        "runs": true
+    },
+    {
+        "file": "plugins/com.salesforce/src/windows/com.salesforce.plugin.accountmanagerProxy.js",
+        "id": "com.salesforce.SalesforceAccountManagerProxy",
+        "runs": true
+    },
+    {
+        "file": "plugins/com.salesforce/src/windows/com.salesforce.plugin.smartstoreProxy.js",
+        "id": "com.salesforce.SalesforceSmartStoreProxy",
+        "runs": true
+    },
+    {
+        "file": "plugins/com.salesforce/src/windows/com.salesforce.plugin.smartsyncProxy.js",
+        "id": "com.salesforce.SalesforceSmartSyncProxy",
+        "runs": true
+    },
+    {
+        "file": "plugins/com.salesforce/src/windows/salesforce.windows.core.js",
+        "id": "com.salesforce.SalesforceCore",
+        "runs": true
+    },
+    {
+        "file": "plugins/cordova-plugin-device/www/device.js",
+        "id": "cordova-plugin-device.device",
+        "clobbers": [
+            "device"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-device/src/windows/DeviceProxy.js",
+        "id": "cordova-plugin-device.DeviceProxy",
+        "merges": [
+            ""
+        ]
+    },
+    {
+        "file": "plugins/phonegap-plugin-push/www/push.js",
+        "id": "phonegap-plugin-push.PushNotification",
+        "clobbers": [
+            "PushNotification"
+        ]
+    },
+    {
+        "file": "plugins/phonegap-plugin-push/src/windows/PushPluginProxy.js",
+        "id": "phonegap-plugin-push.PushPlugin",
+        "merges": [
+            ""
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-whitelist": "1.2.0",
+    "com.salesforce": "4.0.0",
+    "cordova-plugin-device": "1.1.0",
+    "phonegap-plugin-push": "1.4.5"
+}
+// BOTTOM OF METADATA
+});
