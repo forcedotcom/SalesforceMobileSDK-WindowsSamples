0e07c560a243133b9fcd2a9de5caebca1df291b8
Kunal Chitalia <kchitalia@internal.salesforce.com>
2016-07-07 18:37:27 -0700

    Add typescript for smartstore and smartsync

--- Samples/Salesforce.Sample.SmartSyncExplorerHybrid/typescript/salesforce.windows.smartsync.ts
+++ Samples/Salesforce.Sample.SmartSyncExplorerHybrid/typescript/salesforce.windows.smartsync.ts
@@ -23,3 +23,93 @@
  * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  */
+
+/// <reference path="../typings/WinJS-3.0.d.ts"/>
+/// <reference path="../typings/Salesforce.SDK.Hybrid.SmartSync.d.ts"/>
+/// <reference path="../typings/winrt.d.ts"/>
+
+module SmartSyncJS {
+
+    export class SmartSync {
+        constructor() { }
+
+        private manager = Salesforce.SDK.Hybrid.SmartSync;
+
+         // Helper function to handle calls that don't specify isGlobalStore as first argument
+         // If missing, the caller is re-invoked with false prepended to the arguments list and true is returned
+         // Otherwise, false is returned
+         public checkFirstArg(argumentsOfCaller) {
+            // Turning arguments into array
+            var args = Array.prototype.slice.call(argumentsOfCaller);
+            // If first argument is not a boolean
+            if (typeof (args[0]) !== "boolean") {
+                // Pre-pending false
+                args.unshift(false);
+                // Re-invoking function
+                argumentsOfCaller.callee.apply(null, args);
+                return true;
+            }
+            // First argument is a boolean
+            else {
+                return false;
+            }
+        }
+
+        public getInstance() {
+            var syncmanager = this.manager.SyncManager.getInstance();
+            return syncmanager;
+        }
+
+        public syncDown(successCB, errorCB, args) {
+            if (this.checkFirstArg(arguments)) return;
+            var payload = args[1];
+            var syncmanager = this.manager.SyncManager.getInstance(payload.account, null);
+            if (!syncmanager) {
+                errorCB("Error in getting instance for SmartSync");
+            }
+            else {
+                syncmanager.syncDown(payload.target.asJson(), payload.soupName, successCB("Accounts Synced Down"), payload.options);
+
+            }
+
+        }
+
+        public reSync(successCB, errorCB, args) {
+            if (this.checkFirstArg(arguments)) return;
+            var payload = args[1];
+            var syncmanager = this.manager.SyncManager.getInstance();
+            if (!syncmanager) {
+                errorCB("Error in getting instance for SmartSync");
+            }
+            else {
+                syncmanager.reSync(payload.syncId, successCB("Resync Successful"));
+
+            }
+        }
+
+        public syncUp(successCB, errorCB, args) {
+            if (this.checkFirstArg(arguments)) return;
+            var payload = args[1];
+            var syncmanager = this.manager.SyncManager.getInstance();
+            if (!syncmanager) {
+                errorCB("Error in getting instance for SmartSync");
+            }
+            else {
+                syncmanager.syncUp(payload.target, payload.soupName, payload.options, successCB("Accounts Synced Up Successfully"));
+            }
+        }
+
+        public getSyncStatus(successCB, errorCB, args) {
+            if (this.checkFirstArg(arguments)) return;
+            var payload = args[1];
+            var syncmanager = this.manager.SyncManager.getInstance();
+            if (!syncmanager) {
+                errorCB("Error in getting instance for SmartSync");
+            }
+            else {
+                syncmanager.getSyncStatus(payload.syncId);
+                successCB();
+            }
+        }
+    }
+}
